7c36ede2df615f4737df60604ec083c946520b7f
Sandesh157-Th <ssahi563@rku.ac.in>
2023-12-26 20:08:33 +0530

    first commitf

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import BgChanger from './BackgroundChanger'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
